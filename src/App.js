cbd25d25fede5a941d9fcc3d509ec3389f0123f5
lethianhthu158 <20521986@gm.uit.edu.vn>
2024-10-30 23:37:33 +0700

    add all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+
+import Game from "./components/GameCaro/Game";
+import PopUp from "./components/GameCaro/PopUp";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+      // <div> <PopUp winnerName = {"Thu"}></PopUp></div>
+      <div className="App">
+        <Game></Game>
+      </div>
+  
+    
   );
+
 }
 
 export default App;
